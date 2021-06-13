f37c27c1cccaed8225c9e1b54a949e6ede71cccd
able8 <able8@me.com>
2021-06-13 14:55:20 +0800

    add template actions and functions

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -21,9 +21,8 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		app.serverError(w, err)
 		return
 	}
-	for _, snippet := range s {
-		fmt.Fprintf(w, "%v\n", snippet)
-	}
+
+	data := &templateData{Snippets: s}
 
 	files := []string{
 		"./ui/html/home.page.tmpl.html",
@@ -37,7 +36,8 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	err = ts.Execute(w, nil)
+	// Pass in the templateData struct when executing the template.
+	err = ts.Execute(w, data)
 	if err != nil {
 		app.serverError(w, err)
 	}
