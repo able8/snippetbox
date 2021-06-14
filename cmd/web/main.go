fb36e8011910e9ac2cb8333e7c440108b934a924
able8 <able8@me.com>
2021-06-14 11:08:43 +0800

    running a https server

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -64,7 +64,8 @@ func main() {
 	}
 
 	infoLog.Printf("Starting server on %s", *addr)
-	err = srv.ListenAndServe()
+	// err = srv.ListenAndServe()
+	err = srv.ListenAndServeTLS("./tls/cert.pem", "./tls/key.pem")
 	errorLog.Fatal(err)
 }
 
