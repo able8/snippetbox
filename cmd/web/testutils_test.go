8a8adc16cf87ac88e2275612afe218f07d38e2ea
able8 <able8@me.com>
2021-06-17 21:17:01 +0800

    Cookies and Redirections

--- cmd/web/testutils_test.go
+++ cmd/web/testutils_test.go
@@ -4,6 +4,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/http/cookiejar"
 	"net/http/httptest"
 	"testing"
 )
@@ -23,6 +24,23 @@ type testServer struct {
 // Initialize and return a new instance of our custom testServer type.
 func newTestServer(t *testing.T, h http.Handler) *testServer {
 	ts := httptest.NewTLSServer(h)
+
+	// Initialize a new cookie jar
+	jar, err := cookiejar.New(nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Add the cookie jar to the client, so that response cookie are stored
+	// and then sent with subsequent requests.
+	ts.Client().Jar = jar
+
+	// Disable redirect-following for the client.
+	// This func is called after 3xx response is received by the client, and returning
+	// the http.ErrUseLastResponse error forces it to immediately return the received response.
+	ts.Client().CheckRedirect = func(req *http.Request, via []*http.Request) error {
+		return http.ErrUseLastResponse
+	}
 	return &testServer{ts}
 }
 
